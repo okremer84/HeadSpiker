f79e6d1c0937fb8ef7abaad878be1831603ce5be
Omer Kremer <okremer84@gmail.com>
2015-08-22 22:36:54 +0300

    Initial commit

---
+++ desktop/src/com/kremer/headpiker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kremer.headpiker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kremer.headpiker.HeadSpiker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HeadSpiker(), config);
+	}
+}
